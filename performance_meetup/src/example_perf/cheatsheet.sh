c7681cbf32ced7901bf4e4067b5479fe35a586ff
balintm <martina@balint.eu>
2019-05-23 13:12:53 +0100

    Update cheatsheet.sh

--- performance_meetup/src/example_perf/cheatsheet.sh
+++ performance_meetup/src/example_perf/cheatsheet.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-cd "/home/martina/prezentacia/performance_meetup/performance_meetup/"
 PERF_PATH="src/example_perf";
 sudo rm "perf.data*" "*.svg"
 
 cargo build --release
 sudo perf record -F 800 --call-graph dwarf  ./target/release/example_cachegrind
-sudo perf script | /home/martina/work/FlameGraph/stackcollapse-perf.pl |  /home/martina/work/FlameGraph/flamegraph.pl > flame.svg
+sudo perf script | ~/FlameGraph/stackcollapse-perf.pl |  ~/FlameGraph/flamegraph.pl > flame.svg
 
